9e833ee8ff2d2a5163cad91302ce5d42ede0410e
jack heston <jheston2@gmail.com>
2019-02-10 10:43:55 -0700

    asdf

--- 8a.sql
+++ 8a.sql
@@ -1,4 +1,5 @@
-CREATE VIEW AS SELECT c.name, sum(p.amount)
+CREATE VIEW top_five_genres AS (
+SELECT c.name, sum(p.amount)
 FROM payment p
 JOIN category c, film_category fc, inventory i, rental r
 WHERE p.rental_id=r.rental_id
@@ -6,6 +7,6 @@ WHERE p.rental_id=r.rental_id
     AND i.film_id=fc.film_id
     AND fc.category_id=c.category_id
 GROUP BY 1
-LIMIT 5
+LIMIT 5)
 
 
